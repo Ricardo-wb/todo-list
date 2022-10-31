f79a41b614313904d0d40e7e0e1cd78fb08a7026
Ricardo-wb <you@example.com>
2022-10-31 15:32:06 -0300

    function refactoring; html and css tweaks

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
 const formAddTodo = document.querySelector('.form-add-todo')
-const todosContainer = document.querySelector('section')
+const todosContainer = document.querySelector('ul')
 const formSearch = document.querySelector('.form-search')
 
-//ADICIONA TODO
-formAddTodo.addEventListener('submit', event => {
-    event.preventDefault()
-
-    const todo = document.createElement('article')
-
-    const inputValue = event.target.add.value
-
+const setTodo = (todo, inputValue) => {
     todo.setAttribute("data-todo", `${inputValue}`)
     todo.innerHTML = `<span>${inputValue}</span>
     <i class="far fa-trash-alt" data-trash="${inputValue}"></i>`
+}
 
+const insertTodoIntoDom = inputValue => {
+    const todo = document.createElement('li')
+
+    setTodo(todo, inputValue)
     todosContainer.insertAdjacentElement('beforeend', todo)
+}
+
+const addTodo = event => {
+    event.preventDefault()
+
+    const inputValue = event.target.add.value.trim()
+    insertTodoIntoDom(inputValue)
 
     event.target.reset()
-})
+}
 
-//REMOVE TODO
-todosContainer.addEventListener('click', event => {
+const removeTodo = event => {
     const trashDataValue = event.target.dataset.trash
     const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
-    
+
     if (trashDataValue) {
         todo.remove()
     }
-})
+}
+
+const setTodoVisibility = (todo, classToSet, visibility) => visibility ?
+    todo.removeAttribute(classToSet) : todo.classList.add(classToSet)
+
+const filterTodos = (todos, inputSearchValue, returnMatchedTodo) => todos
+    .filter(todo => {
+        const matchedTodos = todo.textContent.toLowerCase().includes(inputSearchValue)
+
+        return returnMatchedTodo ? matchedTodos : !matchedTodos
+    })
+
+const showTodos = (todos, inputSearchValue) =>
+    filterTodos(todos, inputSearchValue, true).forEach(todo =>
+        setTodoVisibility(todo, 'class', true))
 
 
-//FILTRAR TODO
-formSearch.addEventListener('input', event => {
+const hideTodos = (todos, inputSearchValue) =>
+    filterTodos(todos, inputSearchValue, false).forEach(todo =>
+        setTodoVisibility(todo, 'hidden', false))
+
+const filterTodoList = event => {
     event.preventDefault()
 
     const todos = Array.from(todosContainer.children)
-    const inputValue = event.target.value.toLowerCase()
+    const inputSearchValue = event.target.value.trim().toLowerCase()
 
-    todos.forEach(todo => {
-        const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
+    showTodos(todos, inputSearchValue)
+    hideTodos(todos, inputSearchValue)
+}
 
-        if (!matchedTodos) {
-            todo.classList.add('hidden')
-        }
-    })
-
-    todos.forEach(todo => {
-        const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
-
-        if (matchedTodos) {
-            todo.removeAttribute('class')
-        }
-    })
-})
+formAddTodo.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', removeTodo)
+formSearch.addEventListener('input', filterTodoList)
